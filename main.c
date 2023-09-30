ad9c02478ccf33967197f95cafc130f9da75f710
Lucas <lucasmartiniscool@gmail.com>
2023-09-30 11:32:14 -0700

    Finished prompt command

--- main.c
+++ main.c
@@ -12,13 +12,15 @@ gcc -o shell main.c prompt.c
 // Found on https://stackoverflow.com/questions/17271576/clear-screen-in-c-and-c-on-unix-based-system
 #define clear() printf("\033[H\033[J")
 
+char *prompt = "cwushell";
+
 int main()
 {
     char *command;
     char *parsedCommands[30];
 
     while(1) {
-        printPrompt1();
+        fprintf(stderr, "%s", prompt);
 
         command = readCommand();
 
@@ -26,10 +28,8 @@ int main()
         executeCommand(parsedCommands);
 
         free(command);
-
     }
 
-    free(command);
     return 0;
 }
 
@@ -60,6 +60,8 @@ void parseCommand(char *command, char **parsedCommands){
     int i = 0;
 
     while ((token = strtok_r(rest, " ", &rest))) {
+        // Remove newline character if it exists
+        token[strcspn(token, "\n")] = '\0';
         parsedCommands[i] = token;
         i++;
     }
@@ -68,22 +70,9 @@ void parseCommand(char *command, char **parsedCommands){
 
 void executeCommand(char **parsedCommands) {
     if (strcmp(parsedCommands[0], "exit") == 0) {
-        if (parsedCommands[1] != NULL) {
-            char *end;
-            long exitStatus = strtol(parsedCommands[1], &end, 10);
-            if (*end == '\0' && exitStatus >= 0 && exitStatus <= 255) {
-                // It's a valid integer, so we can exit.
-                exit((int)exitStatus);
-            } else {
-                // It's not a valid integer.
-                printf("Invalid exit status: %s\n", parsedCommands[1]);
-            }
-        } else {
-            // No exit status provided, so we exit with status 0.
-            exit(0);
-        }
+        exitCommand(parsedCommands);
     } else if (strcmp(parsedCommands[0], "prompt") == 0) {
-        printf("prompt\n");
+        promptCommand(parsedCommands);
     } else if (strcmp(parsedCommands[0], "fileinfo") == 0) {
         printf("fileinfo\n");
     } else if (strcmp(parsedCommands[0], "osinfo") == 0) {
@@ -91,4 +80,42 @@ void executeCommand(char **parsedCommands) {
     } else {
         printf("system\n");
     }
+} 
+
+void exitCommand(char **parsedCommands){
+    if (parsedCommands[1] != NULL) {
+        char *end;
+        long exitStatus = strtol(parsedCommands[1], &end, 10);
+        if (*end == '\0' && exitStatus >= 0 && exitStatus <= 255) {
+            // It's a valid integer, so we can exit.
+            exit((int)exitStatus);
+        } else {
+            // It's not a valid integer.
+            printf("Invalid exit status: %s\n", parsedCommands[1]);
+        }
+    } else {
+        // No exit status provided, so we exit with status 0.
+        exit(0);
+    }   
+} 
+
+void promptCommand(char **parsedCommands){
+    if (parsedCommands[1] != NULL) {
+        char *newPrompt = malloc(1024); // Allocate memory for newPrompt
+        strcpy(newPrompt, parsedCommands[1]); // Copy the first part of the prompt
+
+        // Concatenate all other parts of the prompt
+        for (int i = 2; parsedCommands[i] != NULL; i++) {
+            strcat(newPrompt, " ");
+            strcat(newPrompt, parsedCommands[i]);
+        }
+
+        free(prompt); // Free the old prompt
+        prompt = newPrompt;
+    } else {
+        free(prompt); // Free the old prompt
+        prompt = strdup("cwushell>"); // Allocate new memory for "cwushell"
+    } 
 }
+
+
--- shell.h
+++ shell.h
@@ -1,11 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-void printPrompt1(void);
-void printPrompt2(void);
-
 char *readCommand(void);
 void parseCommand(char *command, char **parsedCommands);
 void executeCommand(char **parsedCommands);
+void exitCommand(char **parsedCommands);
+void promptCommand(char **parsedCommands);
 
 #endif
